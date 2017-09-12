908549d0ca8bf313f70c22dd1fadee2f980402cd
dustwise <jared.mohney@gmail.com>
2017-09-12 16:59:01 -0600

    implements first pass of lifecycle

--- app.js
+++ app.js
@@ -18,9 +18,8 @@ module.exports.higherOrderFunction = function(args) {
 
   //GENERATE INITIAL GRID BASED ON PROVIDED SIZE
   const gridSize = args;
-  const grid = generateGrid(gridSize);
-
-  generateGrid(grid);
+  const initialGrid = generateGrid(gridSize);
+  const currentGrid = initialGrid;
 
   function generateGrid(size){
     const newGrid = [];
@@ -38,8 +37,44 @@ module.exports.higherOrderFunction = function(args) {
     return newGrid;
   }
 
-  console.log(grid)
   //run life cycle
+
+  updateGrid(currentGrid, initialGrid)
+
+  function updateGrid(current, initial){
+    console.log(current);
+    let newGrid = current.map((row, rowIndex) => {
+      return row.map((cell, cellIndex) => {
+        const cellIsAlive = cell;
+
+        const above = current[rowIndex - 1] ? 
+                      current[rowIndex - 1].slice(cellIndex === 0 ? 0 : cellIndex - 1, cellIndex + 2) : 
+                      [];
+        const below = current[rowIndex + 1] ? 
+                      current[rowIndex + 1].slice(cellIndex === 0 ? 0 : cellIndex - 1, cellIndex + 2) : 
+                      [];
+        const immediate = [ row[cellIndex - 1], row[cellIndex + 1]];
+        const aliveNeighbors = above.concat(below, immediate).filter(neighbor => neighbor === 1).length;
+
+        if(cellIsAlive){
+          if(aliveNeighbors >= 2 && aliveNeighbors <= 3){
+            return 1;
+          } else {
+            return 0;
+          }
+        } else {
+          if(aliveNeighbors === 3){
+            return 1;
+          } else {
+            return 0;
+          }
+        }
+      });
+    });
+    console.log("----------------")
+    console.log(newGrid);
+  }
+
 
   //update screen
 }
