fdc6a27c54c70fea5be4121e41a798992c33435a
dustwise <jared.mohney@gmail.com>
2017-09-12 14:58:37 -0600

    Adds initial testing for validity of input

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
-module.exports = function higherOrderFunction() {
+//Accepted grid sizes.
+const MIN_GRID_SIZE = 5;
+const MAX_GRID_SIZE = 250;
+
+module.exports = function higherOrderFunction(args) {
+  //check provided argument validity
+  if(!args){
+    return "Please, at a minimum, provide a grid size.";
+  }
+
+  if(typeof args !== 'number'){
+    return "Please provide a valid grid size.";
+  }
+
+  if(typeof args === 'number' && (args < MIN_GRID_SIZE || args > MAX_GRID_SIZE)){
+    return "Currently this app only supports grid sizes from 5 to 250.";
+  }
+
 //generate arrays based on size of grid provided
+const gridSize = args[0];
+const grid = Array(gridSize).fill([]);
+
 //populate arrays with seed or randomized data
+const generateSeed = function(grid){
+  for(row of grid){
+    row(gridSize).fill(Math.floor(Math.random()));
+  }
+};
+
+console.log(grid);
 //run life cycle
+
 //update screen
 }
--- test/test.js
+++ test/test.js
@@ -2,8 +2,23 @@ const assert = require('chai').assert;
 const application = require('../app.js');
 
 describe('Application', () => {
+  //DATA VALIDITY
+  it('refuses no grid sizing provided', () => {
+    assert.deepEqual(application(), "Please, at a minimum, provide a grid size.");
+  });
 
-  it('returns desired result', () => {
-    assert.deepEqual(application(arguments), 'desired result')
-  })
+  it('refuses invalid input types', () => {
+    assert.deepEqual(application("jared"), "Please provide a valid grid size.");
+    assert.deepEqual(application({number: 2}), "Please provide a valid grid size.");
+  });
+  
+  it('refuses invalid grid sizing numbers', () => {
+    assert.deepEqual(application(3), "Currently this app only supports grid sizes from 5 to 250.");
+    assert.deepEqual(application(1000), "Currently this app only supports grid sizes from 5 to 250.");
+  });
+
+
+  // it('returns desired result', () => {
+  //   assert.deepEqual(application(arguments), 'desired result')
+  // })
 });
