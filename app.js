dafe2cba5441e9a63e81e98f0cf561780875dab4
dustwise <jared.mohney@gmail.com>
2017-09-12 17:32:06 -0600

    tests for provided seed pass

--- app.js
+++ app.js
@@ -2,23 +2,23 @@
 const MIN_GRID_SIZE = 5;
 const MAX_GRID_SIZE = 250;
 
-module.exports.higherOrderFunction = function(args) {
+module.exports.application = function(size, seed) {
   //CHECK ARG VALIDITY
-  if(!args){
+
+  if(!size){
     return "Please, at a minimum, provide a grid size.";
   }
 
-  if(typeof args !== 'number'){
+  if(typeof size !== 'number'){
     return "Please provide a valid grid size.";
   }
 
-  if(typeof args === 'number' && (args < MIN_GRID_SIZE || args > MAX_GRID_SIZE)){
+  if(typeof size === 'number' && (size < MIN_GRID_SIZE || size > MAX_GRID_SIZE)){
     return "Currently this app only supports grid sizes from 5 to 250.";
   }
 
   //GENERATE INITIAL GRID BASED ON PROVIDED SIZE
-  const gridSize = args;
-  const initialGrid = generateGrid(gridSize);
+  const initialGrid = generateGrid(size);
   const currentGrid = initialGrid;
 
   function generateGrid(size){
@@ -39,10 +39,7 @@ module.exports.higherOrderFunction = function(args) {
 
   //run life cycle
 
-  updateGrid(currentGrid, initialGrid)
-
-  function updateGrid(current, initial){
-    console.log(current);
+  function updateGrid(current){
     let newGrid = current.map((row, rowIndex) => {
       return row.map((cell, cellIndex) => {
         const cellIsAlive = cell;
@@ -71,10 +68,10 @@ module.exports.higherOrderFunction = function(args) {
         }
       });
     });
-    console.log("----------------")
-    console.log(newGrid);
+
+    return newGrid;
   }
 
 
-  //update screen
+  return seed ? updateGrid(seed) : updateGrid(currentGrid);
 }
--- test/test.js
+++ test/test.js
@@ -1,5 +1,21 @@
 const assert = require('chai').assert;
-const application = require('../app.js');
+const { application } = require('../app.js');
+
+const testSeed = [
+  [0,1,0,0,0],
+  [1,0,0,1,1],
+  [1,1,0,0,1],
+  [0,1,0,0,0],
+  [1,0,0,0,1]
+];
+
+const testSeedNextLife = [
+  [0,0,0,0,0],
+  [1,0,1,1,1],
+  [1,1,1,1,1],
+  [0,1,0,0,0],
+  [0,0,0,0,0]
+];
 
 describe('Application', () => {
   //DATA VALIDITY
@@ -11,13 +27,16 @@ describe('Application', () => {
     assert.deepEqual(application("jared"), "Please provide a valid grid size.");
     assert.deepEqual(application({number: 2}), "Please provide a valid grid size.");
   });
-  
+
   it('refuses invalid grid sizing numbers', () => {
     assert.deepEqual(application(3), "Currently this app only supports grid sizes from 5 to 250.");
     assert.deepEqual(application(1000), "Currently this app only supports grid sizes from 5 to 250.");
   });
 
-
+  //CHECK PROVIDED TEST SEED
+  it('provides the expected next lifecycle of test seed', () => {
+    assert.deepEqual(application(5, testSeed), testSeedNextLife);
+  });
   // it('returns desired result', () => {
   //   assert.deepEqual(application(arguments), 'desired result')
   // })
