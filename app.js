c1606cead0985e6c764a02c0be17b018b15970e4
dustwise <jared.mohney@gmail.com>
2017-09-12 18:25:14 -0600

    implements further tests and refactors a bit

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
-//Accepted grid sizes.
+//CONSTANTS AND EXPORT OF CONSTANTS
 const MIN_GRID_SIZE = 5;
 const MAX_GRID_SIZE = 250;
+module.exports.min = MIN_GRID_SIZE;
+module.exports.max = MAX_GRID_SIZE;
 
 module.exports.application = function(size, seed) {
-  //CHECK ARG VALIDITY
-
+  //CHECK SIZE VALIDITY
   if(!size){
-    return "Please, at a minimum, provide a grid size.";
+    return 'Please, at a minimum, provide a grid size.';
   }
 
   if(typeof size !== 'number'){
-    return "Please provide a valid grid size.";
+    return 'Please provide a valid grid size.';
   }
 
   if(typeof size === 'number' && (size < MIN_GRID_SIZE || size > MAX_GRID_SIZE)){
-    return "Currently this app only supports grid sizes from 5 to 250.";
+    return `Currently this app only supports grid sizes from ${MIN_GRID_SIZE} to ${MAX_GRID_SIZE}. Please provide a valid size.`;
+  }
+
+  //CHECK SEED VALIDITY
+  if(seed){
+    const malformedRow = seed.some(row => {
+      return row.length > size || row.length < size;
+    });
+
+    const invalidCell = seed.some(row => {
+      return row.some(cell => cell > 1 || cell < 0);
+    });
+
+    if(seed.length < MIN_GRID_SIZE || seed.length > MAX_GRID_SIZE){
+      return `Currently this app only supports grid sizes from ${MIN_GRID_SIZE} to ${MAX_GRID_SIZE}. Please provide a valid seed.`;
+    }
+
+    if(malformedRow){
+      return 'The amount of columns must equal the amount of rows.';
+    }
+
+    if(invalidCell){
+      return 'Cells should be set to 1 or 0.';
+    }
   }
 
   //GENERATE INITIAL GRID BASED ON PROVIDED SIZE
+
   const initialGrid = generateGrid(size);
-  const currentGrid = initialGrid;
 
   function generateGrid(size){
     const newGrid = [];
@@ -37,21 +61,17 @@ module.exports.application = function(size, seed) {
     return newGrid;
   }
 
-  //run life cycle
+  //RUN LIFE CYCLE 
 
   function updateGrid(current){
     let newGrid = current.map((row, rowIndex) => {
+
       return row.map((cell, cellIndex) => {
         const cellIsAlive = cell;
-
-        const above = current[rowIndex - 1] ? 
-                      current[rowIndex - 1].slice(cellIndex === 0 ? 0 : cellIndex - 1, cellIndex + 2) : 
-                      [];
-        const below = current[rowIndex + 1] ? 
-                      current[rowIndex + 1].slice(cellIndex === 0 ? 0 : cellIndex - 1, cellIndex + 2) : 
-                      [];
+        const above = getNeighbors(current[rowIndex - 1], cellIndex);
+        const below = getNeighbors(current[rowIndex + 1], cellIndex);
         const immediate = [ row[cellIndex - 1], row[cellIndex + 1]];
-        const aliveNeighbors = above.concat(below, immediate).filter(neighbor => neighbor === 1).length;
+        const aliveNeighbors = immediate.concat(above, below).filter(neighbor => neighbor === 1).length;
 
         if(cellIsAlive){
           return aliveNeighbors >= 2 && aliveNeighbors <= 3 ? 1 : 0;
@@ -59,11 +79,16 @@ module.exports.application = function(size, seed) {
           return aliveNeighbors === 3 ? 1 : 0;
         }
       });
+
     });
 
     return newGrid;
   }
 
+  function getNeighbors(row, cellIndex){
+    return row ? row.slice(cellIndex === 0 ? 0 : cellIndex - 1, cellIndex + 2) : [];
+  }
 
-  return seed ? updateGrid(seed) : updateGrid(currentGrid);
+
+  return seed ? updateGrid(seed) : updateGrid(initialGrid);
 }
--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const assert = require('chai').assert;
-const { application } = require('../app.js');
+const { application, min, max } = require('../app.js');
 
 const testSeed = [
   [0,1,0,0,0],
@@ -17,6 +17,29 @@ const testSeedNextLife = [
   [0,0,0,0,0]
 ];
 
+const malformedShortSeed = [
+  [0,0,0,0],
+  [1,0,1,1,1],
+  [1,1,1,1,1,0,1,1],
+  [0,1]
+];
+
+const malformedRowSeed = [
+  [0,0,0,0],
+  [1,0,1,1,1],
+  [1,1,1,1,1,0,1,1],
+  [0,1],
+  [0,0,0,0,0,1,1]
+];
+
+const invalidCellsSeed = [
+  [0,1,3,0,0],
+  [1,0,0,1,1],
+  [1,1,0,0,1],
+  [0,1,7,0,0],
+  [8,0,0,3,1]
+];
+
 describe('Application', () => {
   //DATA VALIDITY
   it('refuses no grid sizing provided', () => {
@@ -29,15 +52,25 @@ describe('Application', () => {
   });
 
   it('refuses invalid grid sizing numbers', () => {
-    assert.deepEqual(application(3), "Currently this app only supports grid sizes from 5 to 250.");
-    assert.deepEqual(application(1000), "Currently this app only supports grid sizes from 5 to 250.");
+    assert.deepEqual(application(3), `Currently this app only supports grid sizes from ${min} to ${max}. Please provide a valid size.`);
+    assert.deepEqual(application(1000), `Currently this app only supports grid sizes from ${min} to ${max}. Please provide a valid size.`);
+  });
+
+  //CHECK SEED VALIDITY
+  it('refuses malformed sized seed', () => {
+    assert.deepEqual(application(5, malformedShortSeed), `Currently this app only supports grid sizes from ${min} to ${max}. Please provide a valid seed.`);
+  });
+
+  it('refuses malformed seed rows', () => {
+    assert.deepEqual(application(5, malformedRowSeed), 'The amount of columns must equal the amount of rows.');
+  });
+
+  it('refuses invalid cells', () => {
+    assert.deepEqual(application(5, invalidCellsSeed), 'Cells should be set to 1 or 0.');
   });
 
   //CHECK PROVIDED TEST SEED
   it('provides the expected next lifecycle of test seed', () => {
     assert.deepEqual(application(5, testSeed), testSeedNextLife);
   });
-  // it('returns desired result', () => {
-  //   assert.deepEqual(application(arguments), 'desired result')
-  // })
 });
